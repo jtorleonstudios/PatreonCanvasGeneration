32094a300064174f166d4529e76597de6a9851de
eli <scs.torleon@gmail.com>
2023-01-23 21:17:40 +0100

    default commit 2.0.0

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,8 @@
-console.log(0)
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var path = require("path");
+var outputPath = path.join(__dirname, "..", "out", "test.text");
+var writeStream = fs.createWriteStream(outputPath);
+writeStream.write("hello world");
+writeStream.end();
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log(0)
+import * as fs from "fs"
+import path = require("path");
+
+const outputPath: string = path.join(__dirname, "..", "out", "test.text")
+
+var writeStream = fs.createWriteStream(outputPath);
+writeStream.write("hello world");
+writeStream.end();
